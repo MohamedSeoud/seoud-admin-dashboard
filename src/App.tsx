7a129cbbd110ab1e3ff5c8bf02d85237c577dc0f
Mohammed Seoud <m.seoud42@gmail.com>
2023-10-04 04:02:34 +0300

    seoud-admin-dashboard

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Routes from './routes'
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className=' bg-black text-white w-screen h-screen bg-black'>
+    <Routes/>
+    <div className=' text-red-600 bg-black w-full text  text-center flex justify-center items-center font-bold'>
+      ddssdvs
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div className=' text-white'>
+        <span  className="  mt-32">ffdfd</span>
+      
+    </div>
+  )
+}
+
+export default Home
